dfdab1f06ec0f8b8e335fef7e38b40f662dcd0e9
AdnanRashied <adnan.rashied@gmail.com>
2025-07-16 14:46:40 -0400

    Update clearSession.ts

--- shelter-app/helper/clearSession.ts
+++ shelter-app/helper/clearSession.ts
@@ -1,8 +1,13 @@
 import * as SecureStore from "expo-secure-store";
 
+export const SECURE_STORAGE_KEYS = {
+  USER_TOKEN: "user-token",
+};
+
 export const clearSession = async (): Promise<void> => {
   try {
-    await SecureStore.deleteItemAsync("user-token");
+    await SecureStore.deleteItemAsync(SECURE_STORAGE_KEYS.USER_TOKEN);
+    console.log("Session cleared.");
   } catch (err) {
     console.error("Failed to clear session:", err);
   }
