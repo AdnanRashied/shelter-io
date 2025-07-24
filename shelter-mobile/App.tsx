af2fadd849b407a1dcc4095ef48ac8eb7e2288b5
AdnanRashied <adnan.rashied@gmail.com>
2025-07-23 20:28:36 -0400

    Convert shelter-mobile from sub to normal

---
+++ shelter-mobile/App.tsx
@@ -0,0 +1,26 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import LoginScreen from "./screens/LoginScreen";
+
+import "./global.css"; 
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
---
+++ shelter-mobile/screens/LoginScreen.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+import '../global.css';
+
+export default function LoginScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to LoginScreen!!
+      </Text>
+    </View>
+  );
+}
